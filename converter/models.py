957b8cb8eb5653f0f0a3f40bc747bfc547dd4836
zealfire_ <alok.pandey@kuliza.com>
2016-08-01 14:51:20 +0530

    initial schema of models to be used added.

--- converter/models.py
+++ converter/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from audiofield.fields import AudioField
+
+import os.path
+
+
+class Audio(ModelBase):
+    """
+    table to store otp related information
+    """
+    voice = models.CharField(max_length=254)
+    text = models.CharField(max_length=254)
+
+    audio_file = AudioField(upload_to='your/upload/dir', blank=True,
+                        ext_whitelist=(".mp3", ".wav", ".ogg"),
+                        help_text=("Allowed type - .mp3, .wav, .ogg"))
+
+	# Add this method to your model
+	def audio_file_player(self):
+	    """audio player tag for admin"""
+	    if self.audio_file:
+	        file_url = settings.MEDIA_URL + str(self.audio_file)
+	        player_string = '<ul class="playlist"><li style="width:250px;">\
+	        <a href="%s">%s</a></li></ul>' % (file_url, os.path.basename(self.audio_file.name))
+	        return player_string
+
+	audio_file_player.allow_tags = True
+	audio_file_player.short_description = ('Audio file player')
